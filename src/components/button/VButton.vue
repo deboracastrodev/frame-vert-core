e04a34062e30a8857e09407d1551136315c465b0
Jonathas Kranmer <jonathas@vert-capital.com>
2022-08-05 13:54:02 -0300

    acessibilidade btn e organizacao sass

--- src/components/button/VButton.vue
+++ src/components/button/VButton.vue
@@ -1,48 +1,34 @@
 <template>
-  <!-- <button class="v-btn--helper"></button> <br /> -->
   <button :class="[setSizeClass, setStatus]"><slot /></button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-enum EStatus {
-  helper = "helper",
-  wargning = "wargning",
-  success = "success",
-  default = "default",
-}
-
-enum ESize {
-  sm = "v-btn-sm",
-  md = "v-btn-md",
-  lg = "v-btn-lg",
-}
-
 export default defineComponent({
   name: "VButton",
   props: {
     size: {
+      type: String,
       default: "md",
-      validator: (item: string) => Object.keys(ESize).includes(item),
     },
     outline: {
       type: Boolean,
       default: false,
     },
     status: {
+      type: String,
       default: "helper",
-      validator: (item: string) => Object.keys(EStatus).includes(item),
     },
   },
   computed: {
     setSizeClass(): string {
-      return ESize[this.size];
+      return `v-btn--${this.size}`;
     },
     setStatus(): string {
       const status = this.outline
-        ? `v-btn__outline--${EStatus[this.status]}`
-        : `v-btn--${EStatus[this.status]}`;
+        ? `v-btn__outline--${this.status}`
+        : `v-btn--${this.status}`;
       return status;
     },
   },
