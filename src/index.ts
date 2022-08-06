563e3c914721b3414bed7504906053d69984196a
Debora Castro <debora@vert-capital.com>
2022-08-06 12:27:19 -0300

    build: teste de exportação

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+//export multiple components from one file
 
 import VTable from './components/table/VTable.vue'
 import VButton from './components/button/VButton.vue'
 
-export default { VTable, VButton }
-export * from './components/table/VTable.vue'
-export * from './components/button/VButton.vue'
+export { VTable, VButton }
+
+// export * from './components/table/VTable.vue'
+// export * from './components/button/VButton.vue'
