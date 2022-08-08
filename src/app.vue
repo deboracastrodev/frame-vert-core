328684a0cb15b5809c9584bf307f2b4d62443236
Jonathas Kranmer <jonathas@vert-capital.com>
2022-08-08 17:02:17 -0300

    criacao vtab eixo x

--- src/app.vue
+++ src/app.vue
@@ -29,6 +29,33 @@
       <v-button icon="check" status="success">Sucesso</v-button>
       <v-button icon="info" status="primary">Padrão</v-button>
     </div>
+    <br />
+    <v-tab>
+      <template #header>
+        <v-tab-header tabTo="tab1" actived>Exemplo 1</v-tab-header>
+        <v-tab-header tabTo="tab2">Exemplo 2</v-tab-header>
+      </template>
+      <div>
+        <v-tab-content contentId="tab1">
+          <h2>Le po pul</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+            numquam laboriosam debitis, cupiditate dolores tenetur libero?
+            Provident, dicta iste expedita animi dolorum, temporibus excepturi
+            maxime, ea dolorem eveniet consequuntur rerum!
+          </p>
+        </v-tab-content>
+        <v-tab-content contentId="tab2">
+          <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo sint
+            sunt suscipit molestiae nemo laudantium, facere itaque atque
+            dolorum, quo quasi est. Possimus explicabo alias aspernatur eveniet,
+            voluptas vel aperiam!
+          </p>
+        </v-tab-content>
+      </div>
+    </v-tab>
   </div>
 </template>
 
@@ -36,12 +63,19 @@
 import { defineComponent } from "vue";
 import VTable from "./components/table/VTable.vue";
 import VButton from "./components/button/VButton.vue";
+import VTab from "./components/tab/VTab.vue";
+import VTabContent from "./components/tab/VTabContent.vue";
+import VTabHeader from "./components/tab/VTabHeader.vue";
+import VTabHeader from "./components/tab/VTabHeader.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     VTable,
     VButton,
+    VTab,
+    VTabContent,
+    VTabHeader,
   },
 });
 </script>
