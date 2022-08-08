e5e172d571e966699f0d719357570e4d5d416de3
Debora Castro <debora@vert-capital.com>
2022-08-08 12:58:14 -0300

    build: teste renderizacao

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
 /* eslint-disable */
-import { App as Application, Plugin } from "vue";
-import * as components from "./components/index";
-import { setVueInstance } from "./utils/config/index";
+import { VTable, VButton } from './components';
 
-const install: Exclude<Plugin["install"], undefined> = (
-  instance: Application
-) => {
-  setVueInstance(instance);
-  for (const componentKey in components) {
-    instance.use((components as any)[componentKey]);
-  }
-};
-
-export default install;
+export default { VTable, VButton };
 
 export * from "./components";
